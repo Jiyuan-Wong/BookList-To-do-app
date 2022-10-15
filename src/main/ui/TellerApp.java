7f17ae2cd7efcbc4313455f8bf386f8c6e9adc2b
Jiyuan <jiyuan21@student.ubc.ca>
2022-10-15 15:51:12 -0700

    Fifth try in 15:51 10/15

--- src/main/ui/TellerApp.java
+++ src/main/ui/TellerApp.java
@@ -1,24 +1,36 @@
 package ui;
 
-import model.Booklist;
 import model.Books;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TellerApp {
-    ArrayList<Books> booklist = new ArrayList<>();
 
+    ArrayList<Books> booklist = new ArrayList<>();
 
     public void main(String[] args) {
         int num = userinterface();
         if (num == 1) {
             addMethod();
+        } else if (num == 2) {
+            view();
+        } else if (num == 3) {
+            mark();
+        } else if (num == 4) {
+            viewSingleBook();
+        } else if (num == 5) {
+            remove();
+        } else if (num == 6) {
+            System.out.println("Goodbye!");
+        } else {
+            System.out.println("Invalid input, please try again! ");
+            userinterface();
         }
     }
 
 
-    public static int userinterface() {
+    public int userinterface() {
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to 'Reading list helper'! How can I help you?");
         System.out.println("1. Add a book to your booklist.");
@@ -26,10 +38,8 @@ public class TellerApp {
         System.out.println("3. Mark a book as read.");
         System.out.println("4. View your book.");
         System.out.println("5. Remove a book from your booklist.");
-        System.out.println("6. Read a book.");
-        System.out.println("7. Quit");
-        int optionEntered = in.nextInt();
-        return optionEntered;
+        System.out.println("6. Quit");
+        return in.nextInt();
     }
 
     public void addMethod() {
@@ -38,10 +48,44 @@ public class TellerApp {
         String name = in.nextLine();
         String type = in.nextLine();
         String category = in.nextLine();
-        Books book = new Books(name, type,category);
+        Books book = new Books(name, type, category);
         booklist.add(book);
-        System.out.println("The book has been read.");
+        System.out.println("The book has been added.");
     }
 
+    public void view() {
+        for (int i = 0; i < booklist.size(); i++) {
+            System.out.println(booklist.get(i) + "  ");
+        }
+    }
+
+    public void mark() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Choose the index of the book you want to mark: ");
+        int index = in.nextInt();
+        booklist.get(index).read();
+        System.out.println("The book has been read");
+    }
+
+    public void viewSingleBook() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Choose the index of the book you want to view: ");
+        int index = in.nextInt();
+        booklist.get(index);
+    }
+
+    public void remove() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Choose the index of the book you want to remove: ");
+        int index = in.nextInt();
+        booklist.remove(index);
+        System.out.println("The book have been removed.");
+    }
 
 }
+
+
+
+
+
+
