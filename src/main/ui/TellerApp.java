91f774ec86e9cb67d39f973cd8df6b590286f185
Jiyuan <jiyuan21@student.ubc.ca>
2022-10-15 14:07:05 -0700

    Fourth try in 14:46 10/15

--- src/main/ui/TellerApp.java
+++ src/main/ui/TellerApp.java
@@ -1,15 +1,46 @@
 package ui;
 
+import model.Booklist;
 import model.Books;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TellerApp {
-    private Books books;
-    private Scanner input;
+    ArrayList<Books> booklist = new ArrayList<>();
 
-    public static void main(String[] args) {
 
+    public void main(String[] args) {
+        int num = userinterface();
+        if (num == 1) {
+            addMethod();
+        }
+    }
+
+
+    public static int userinterface() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to 'Reading list helper'! How can I help you?");
+        System.out.println("1. Add a book to your booklist.");
+        System.out.println("2. View your booklist.");
+        System.out.println("3. Mark a book as read.");
+        System.out.println("4. View your book.");
+        System.out.println("5. Remove a book from your booklist.");
+        System.out.println("6. Read a book.");
+        System.out.println("7. Quit");
+        int optionEntered = in.nextInt();
+        return optionEntered;
+    }
+
+    public void addMethod() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the name, type and catrgory ");
+        String name = in.nextLine();
+        String type = in.nextLine();
+        String category = in.nextLine();
+        Books book = new Books(name, type,category);
+        booklist.add(book);
+        System.out.println("The book has been read.");
     }
 
 
