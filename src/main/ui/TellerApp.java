fa3e9680f7159a8dfb17d8001734e4f42f26c099
Jiyuan <jiyuan21@student.ubc.ca>
2022-10-15 16:33:24 -0700

    Sixth try in 16:33 10/15

--- src/main/ui/TellerApp.java
+++ src/main/ui/TellerApp.java
@@ -7,9 +7,13 @@ import java.util.Scanner;
 
 public class TellerApp {
 
-    ArrayList<Books> booklist = new ArrayList<>();
+    public static final ArrayList<Books> booklist = new ArrayList<>();
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
+        main();
+    }
+
+    public static void main() {
         int num = userinterface();
         if (num == 1) {
             addMethod();
@@ -25,67 +29,69 @@ public class TellerApp {
             System.out.println("Goodbye!");
         } else {
             System.out.println("Invalid input, please try again! ");
-            userinterface();
         }
     }
 
 
-    public int userinterface() {
+    public static int userinterface() {
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to 'Reading list helper'! How can I help you?");
-        System.out.println("1. Add a book to your booklist.");
-        System.out.println("2. View your booklist.");
+        System.out.println("1. Add a book to your book list.");
+        System.out.println("2. View your book list.");
         System.out.println("3. Mark a book as read.");
         System.out.println("4. View your book.");
-        System.out.println("5. Remove a book from your booklist.");
+        System.out.println("5. Remove a book from your book list.");
         System.out.println("6. Quit");
         return in.nextInt();
     }
 
-    public void addMethod() {
+    public static void addMethod() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the name, type and catrgory ");
+        System.out.println("Enter the name, type and category ");
         String name = in.nextLine();
         String type = in.nextLine();
         String category = in.nextLine();
         Books book = new Books(name, type, category);
         booklist.add(book);
         System.out.println("The book has been added.");
+        userinterface();
     }
 
-    public void view() {
-        for (int i = 0; i < booklist.size(); i++) {
-            System.out.println(booklist.get(i) + "  ");
+    public static void view() {
+        for (Books books : booklist) {
+            System.out.println("Name: " + books.getBookName() + " Type: "
+                    + books.getBookType() + " Category: " + books.getCategoryOfTheBook());
         }
     }
 
-    public void mark() {
+    public static void mark() {
         Scanner in = new Scanner(System.in);
         System.out.println("Choose the index of the book you want to mark: ");
         int index = in.nextInt();
-        booklist.get(index).read();
-        System.out.println("The book has been read");
+        if (0 < booklist.size() && index <= booklist.size()) {
+            booklist.get(index).read();
+            System.out.println("The book has been read");
+        }
     }
 
-    public void viewSingleBook() {
+    public static void viewSingleBook() {
         Scanner in = new Scanner(System.in);
         System.out.println("Choose the index of the book you want to view: ");
         int index = in.nextInt();
-        booklist.get(index);
+        if (0 < booklist.size() && index <= booklist.size()) {
+            System.out.println("Name: " + booklist.get(index).getBookName() + " Type: "
+                    + booklist.get(index).getBookType() + " Category: " + booklist.get(index).getCategoryOfTheBook());
+        }
     }
 
-    public void remove() {
+    public static void remove() {
         Scanner in = new Scanner(System.in);
         System.out.println("Choose the index of the book you want to remove: ");
         int index = in.nextInt();
-        booklist.remove(index);
-        System.out.println("The book have been removed.");
+        if (0 < booklist.size() && index <= booklist.size()) {
+            booklist.remove(index);
+            System.out.println("The book have been removed.");
+        }
     }
 
 }
-
-
-
-
-
-
